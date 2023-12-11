18ab4d974004de5497d6d580d0c233befc2748d3
Yogesh Bacchuwar <139001635+yogesh120402@users.noreply.github.com>
2023-12-11 14:42:36 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,31 +1,12 @@
 <?php
-
-// This pulls the MongoDB driver from the vendor folder
 require_once '../vendor/autoload.php';
-
-// Connect to MongoDB Database
 $databaseConnection = new MongoDB\Client;
-
-// Connecting to a specific database in MongoDB
 $myDatabase = $databaseConnection->myDB1;
-
-// Connecting to our MongoDB Collections
 $userCollection = $myDatabase->users;
-
-// Start the session
 session_start();
-// If the form is submitted
-// if (isset($_POST['loginForm'])) {
-
     $username = $_POST['username'];
     $password = $_POST['password'];
-
-    // Fetch user from MongoDB Users Collection
     $user = $userCollection->findOne(['Firstname' => $username]);
-//   echo $username;
-//   echo $user;
-//   echo $(sha1($password));
-//   echo $user['Password'];
     if ($user && password_verify(sha1($password), $user['Password'])) {
 
         // Create a session
@@ -40,5 +21,4 @@ session_start();
         <center><a href="../index.html">Try Again</a></center>
         <?php
     }
-//}
 ?>
