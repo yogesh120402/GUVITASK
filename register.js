ce062b29f9715e740308d8cc3dbc135548603ebb
Yogesh Bacchuwar <139001635+yogesh120402@users.noreply.github.com>
2023-12-11 14:45:42 +0530

    Update register.js

--- register.js
+++ register.js
@@ -1,16 +1,13 @@
 function submitForm() {
-    // Handle form submission using jQuery AJAX
     $.ajax({
       type: 'POST',
-      url: './php/register.php', // Replace with the actual path to your signup.php file
+      url: './php/register.php', 
       data: $('#signupForm').serialize(),
       success: function (response) {
-        // Handle the response from the server 
         window.location.href = 'login.html';
         console.log(response);
       },
       error: function (error) {
-        // Handle errors (e.g., display an error message)
         console.error(error);
       }
       
