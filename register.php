ad1a4d2a77fbd1c6f622dfeec4b0ef71525dbeb4
Yogesh Bacchuwar <139001635+yogesh120402@users.noreply.github.com>
2023-12-11 14:46:55 +0530

    Update register.php

--- register.php
+++ register.php
@@ -1,33 +1,18 @@
 <?php
-
-// This pulls the MongoDB driver from the vendor folder
 require_once  '../vendor/autoload.php';
-//echo "hello";
-// Connect to MongoDB Database
 $databaseConnection = new MongoDB\Client;
-
-// Connecting to a specific database in MongoDB
 $myDatabase = $databaseConnection->myDB1;
-
-// Connecting to our MongoDB Collections
 $userCollection = $myDatabase->users;
-//echo $_POST['email'];
-
-
     $username = $_POST['username'];
     $email = $_POST['email'];
     $password = $_POST['password'];
     $hashed_password = sha1($password);
-
     $data = array(
         "Firstname" => $username,
         "Email" => $email,
         "Password" => $hashed_password
     );
-
-    // Insert into MongoDB Users Collection
     $insert = $userCollection->insertOne($data);
-
     if($insert){
         ?>
         <center><h4 style="color: green;">Successfully Registered</h4></center>
@@ -39,6 +24,4 @@ $userCollection = $myDatabase->users;
         <center><a href="signup.html">Try Again</a></center>
         <?php
     }
-
-
 ?>
